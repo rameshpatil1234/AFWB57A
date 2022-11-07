be88c22b1835b4d09b4d71798b4086f737ef60bc
ramesh patil <ramesh.patil223@gmail.com>
2022-11-07 21:18:56 +0530

    created invalid login page

--- test_scripts/test_validlogin.py
+++ test_scripts/test_validlogin.py
@@ -1,22 +1,32 @@
 import time
 
-import time
+import pytest
+
 from generic.base_test import BaseTest
+from generic.utility import Excel
 from pages.login_page import LoginPage
 from pages.home_page import HomePage
 
 class TestValidLogin(BaseTest):
-
+    @pytest.mark.run(order=1)
     def test_validlogin(self):
-        loginpage=LoginPage(self.driver)
-        # 1.Enter valid username
-        loginpage.set_username("admin")
+        try:
+            un=Excel.get_cellvalue("../data/input.xlsx","ValidLogin",2,1)
+            pw=Excel.get_cellvalue("../data/input.xlsx","ValidLogin",2,2)
+        except:
+            un = Excel.get_cellvalue("data/input.xlsx", "ValidLogin", 2, 1)
+            pw = Excel.get_cellvalue("data/input.xlsx", "ValidLogin", 2, 2)
+        loginpage = LoginPage(self.driver)
+        # 1. Enter valid username
+        loginpage.set_username(un)
         # 2. Enter valid password
-        loginpage.set_password("manager")
+        loginpage.set_password(pw)
         # 3. click on login button
         loginpage.click_loginbutton()
-        # 4. verify that homepage is displayed
+        # 4. verify that home page is displayed
         homepage=HomePage(self.driver)
         result=homepage.verify_homepage_is_displayed(self.wait)
         assert result
+
+
 
